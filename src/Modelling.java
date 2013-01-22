6f36a97261a70bfe507ae5dab51b5143e7196c87
Martin <mail@azgul.com>
2013-01-22 20:53:07 +0100

    Expanded to being able to calculate combined variance

--- src/Modelling.java
+++ src/Modelling.java
@@ -3,11 +3,18 @@ public class Modelling {
 	public static void main(String[] args) {
 		double[] px = {.1, .4, .5};
 		double[] x = {0, 1, 2};
-		
+		double[] py = {.1, .4, .5};
+		double[] y = {0, 1, 2};
+
 		double EX = median(px, x);
-		
+		double EY = median(py, y);
+
 		double VarX = variance(px, x, EX);
+		double VarY = variance(py, y, EY);
 		
+		int a = 1;
+		int b = -3;
+		double VarXY = variance(a, b, VarX, VarY);
 	}
 	
 	private static double median(double[] px, double[] x) {
@@ -23,11 +30,21 @@ public class Modelling {
 		
 		res = res.substring(0, res.length()-2);
 		
-		System.out.println(res + "= " + result);
+		System.out.println(res + "= " + result + "\n");
 		
 		return result;
 	}
 	
+	private static double variance(int a, int b, double VarX, double VarY) {
+		double result = 0;
+		
+		System.out.println(String.format("Calculating combined variance of: Var(%sX + %sY)", a, b));
+		
+		result = Math.pow(a, 2) * VarX + Math.pow(b, 2) * VarY;
+		System.out.println(String.format("%s^2 * Var(X) + %s^2 * Var(Y) = %s\n", a, b, result));
+		
+		return result;
+	}
 	
 	private static double variance(double[] px, double[] x, double EX) {
 		double result = 0;
@@ -40,7 +57,7 @@ public class Modelling {
 			res += String.format("(%s - %s)^2 * %s", x[i], EX, px[i]);
 		}
 
-		System.out.println(res + "= " + result);
+		System.out.println(res + "= " + result + "\n");
 		
 		return result;
 	}
